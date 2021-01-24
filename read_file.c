67a866966790d721d0800ec5145e7148b98d1613
efulp <31475571+efulp@users.noreply.github.com>
2021-01-24 18:29:09 -0500

    Update read_file.c

--- read_file.c
+++ read_file.c
@@ -3,7 +3,7 @@
  * This program reads a text file and prints the contents to the screen.
  *
  * @author Nirre Pluf {@literal <pluf@wfu.edu>}
- * @date Sept. 20, 2020
+ * @date Jan. 28, 2021
  * @assignment Lab 0
  * @course CSC 250
  **/
